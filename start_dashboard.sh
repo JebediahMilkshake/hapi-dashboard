dce31b5bf6981e8950ab1d696a968619a20a87b6
JebediahMilkshake <devin.g.lanz1@gmail.com>
2026-02-08 00:16:21 -0500

    fixed directory location

--- start_dashboard.sh
+++ start_dashboard.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/pi/Documents/dashboard # Change to your actual path
+cd /home/pi/Documents/HAPi-Dashboard # Change to your actual path
 git pull git@github.com:JebediahMilkshake/HAPi-Dashboard.git
 /usr/bin/python3 app.py &
